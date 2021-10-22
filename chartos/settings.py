c3576b1774e051c336bbca5c37f96f7974e42fb6
Victor "multun" Collod <victor.collod@epita.fr>
2021-10-22 17:51:29 +0200

    settings: fix psql_settings

--- chartos/settings.py
+++ chartos/settings.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseSettings
 from functools import lru_cache
 
@@ -5,14 +6,14 @@ from functools import lru_cache
 class Settings(BaseSettings):
     config_path: str = "layer.yml"
     psql_dsn: str
-    psql_user: str
-    psql_password: str
+    psql_user: Optional[str] = None
+    psql_password: Optional[str] = None
 
     def psql_settings(self):
         return {
-            "psql_dsn": self.psql_dsn,
-            "psql_user": self.psql_user,
-            "psql_password": self.psql_password,
+            "dsn": self.psql_dsn,
+            "user": self.psql_user,
+            "password": self.psql_password,
         }
 
 
